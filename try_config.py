ebace096d7824988491ce3e00f683378fbe7670c
ungaminga <loljkpro@cock.li>
2018-06-13 12:20:35 +0300

    Add homedir finder for try_config.py.

--- try_config.py
+++ try_config.py
@@ -4,6 +4,8 @@ Created on Wed Jun 13 11:41:20 2018
 
 @author: loljkpro
 """
+import os
+print(os.path.expanduser('~'))
 
 from twitch import TwitchClient
 client = TwitchClient(client_id="client_id", oauth_token="oauth_token")
